46a57eb669989daeb7a18f9340bc2636e0ea9ee0
MatiasVillegas23 <mati.villegas@gmail.com>
2019-11-05 16:42:36 -0300

    correcciones leves

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,11 +16,13 @@ public class Main {
 		
 		AgroBusiness a = new AgroBusiness("Hacienda Pepito");
 		AgroBusiness b = new AgroBusiness("Hacienda Cholito");
-		Animal v1 = new Animal("vaca", 1, 24, 300, "Holando Argentino", 'h', false, 5);
-		Animal v2 = new Animal("vaca2", 2, 11, 400, "Holando Argentino", 'h', true, 3);
-		Animal v3 = new Animal("vaca3", 3, 15, 500, "Holando Argentino", 'h', false, 5);
-		Animal v4 = new Animal("vaca4", 4, 36, 600, "Holando Argentino", 'm', false, 0);
-		//Truck t = new Truck(1, 50, f);
+		Animal v1 = new Animal("vaca", 1, 24, 300, "aberdeen angus", 'h', false, 5);
+		Animal v2 = new Animal("vaca2", 2, 11, 400, "aberdeen angus", 'h', true, 3);
+		Animal v3 = new Animal("vaca3", 3, 15, 500, "aberdeen angus", 'h', false, 5);
+		Animal v4 = new Animal("vaca4", 4, 7, 600, "aberdeen angus", 'm', false, 0);
+		Animal v5 = new Animal("vaca5", 5, 10, 250, "aberdeen angus", 'h', false, 5);
+
+		Truck t = new Truck(1, 50, f3);
 		Ministry m = new Ministry("Spain");
 		Category d = new Category("ando2", f5);
 		m.addCategory(d);
@@ -32,9 +34,15 @@ public class Main {
 		a.addAnimal(v4);
 		
 		listAnimalsCategories(m, v2);
+		System.out.println(readyToSell(a, f3));
+		System.out.println(a.sellAnimals(t));
+		System.out.println(t.getAnimals());
+		
+		
+		
 	}
 	
-	private static boolean readyToSell(AgroBusiness a, UnitaryFilter f){
+	public static boolean readyToSell(AgroBusiness a, UnitaryFilter f){
 		ArrayList<Establishment> aux = a.getEstablishments();
 		for (int i = 0; i < aux.size(); i++) {
 			if (!(a.isOkToSell(f))) {
@@ -44,7 +52,7 @@ public class Main {
 		return true;
 	}
 
-	private static void listAnimalsCategories(Ministry m, Animal a) {
+	public static void listAnimalsCategories(Ministry m, Animal a) {
 		ArrayList<String> aux = new ArrayList<String>();
 		aux = m.listCategories(a);
 		System.out.println("Las categorias del animal "+a.getName()+" son:");
